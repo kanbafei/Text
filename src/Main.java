4652da3b59f00837c695d04c0315dfb8b9a65673
kanbafei <1349024035@qq.com>
2023-02-23 17:39:24 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    static double PI=3.14;
+    public static void main(String[] args) {
+        double r=5;
+        double area=PI*r*r;
+        System.out.println("area = " + area);
+    }
+}
